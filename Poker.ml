8a17b1d705e5d11cc19d7bf4a96b2fdbdf1c3380
svensay <svensay@gmail.com>
2017-11-20 16:55:28 +0100

    Petite modif dans comb

--- Poker.ml
+++ Poker.ml
@@ -3,12 +3,12 @@ type valeur = Point of int | Valet | Dame | Roi ;;
 type carte = Carte of valeur * couleur ;;
 type donne = Main of carte*carte;;
 type table = Flop of carte*carte*carte | Turn of carte*carte*carte*carte | River of carte*carte*carte*carte*carte;;
-type com = QuinteFlush of valeur
-	   |Carre of valeur*valeur(*1er valeur -> suite de 4, 2éme valeur -> en cas d'égalité, compare avec cette valeur*)
-	   |Full of valeur*valeur(*1er valeur -> suite de 3, 2éme valeur -> en cas d'égalité, compare suite de 2*)
+type comb = QuinteFlush of valeur
+	   |Carre of valeur(*suite de 4*)
+	   |Full of valeur(*suite de 3*)
 	   |Couleur of valeur*valeur*valeur*valeur*valeur (*en cas d'égalité faut verifier la carte suivante*)
 	   |Suite of valeur
-	   |Brelan of valeur*valeur*valeur
+	   |Brelan of valeur
 	   |DoublePair of valeur*valeur*valeur (*1er valeur -> 1er paire, 2éme valeur -> 2éme paire, 3éme -> derniére carte*)
 	   |Pair of valeur*valeur*valeur*valeur(*1er valeur -> 1er paire, le reste des valeurs sont les cartes qu'on compare en cas d'égalité*)
 	   |CarteHaute of valeur*valeur*valeur*valeur*valeur(*Compare la meilleur valeur et en cas d'égalité on regarde la prochaine plus forte et ainci de suite*);;
