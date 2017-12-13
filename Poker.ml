c43fdd23b4423a797c2b3de552afcd10fe285ed1
Anthony <anthony.garcia01@etu.univ-paris-diderot.fr>
2017-12-13 10:02:35 +0100

    Renommage

--- Poker.ml
+++ Poker.ml
@@ -1,14 +1,14 @@
 type couleur = Pique | Coeur | Carreau | Trefle ;;
-type valeur = Point of int | Valet | Dame | Roi ;;
-type carte = Carte of valeur * couleur ;;
+type rang = Valeur of int;;
+type carte = Carte of rang * couleur ;;
 type donne = Main of carte*carte;;
 type table = Flop of carte*carte*carte | Turn of carte*carte*carte*carte | River of carte*carte*carte*carte*carte;;
-type comb = QuinteFlush of valeur
-	   |Carre of valeur*valeur(*suite de 4*)
-	   |Full of valeur*valeur(*suite de 3*)
-	   |Couleur of valeur*valeur*valeur*valeur*valeur (*en cas d'égalité faut verifier la carte suivante*)
-	   |Suite of valeur
-	   |Brelan of valeur*valeur*valeur
-	   |DoublePair of valeur*valeur*valeur (*1er valeur -> 1er paire, 2éme valeur -> 2éme paire, 3éme -> derniére carte*)
-	   |Pair of valeur*valeur*valeur*valeur(*1er valeur -> 1er paire, le reste des valeurs sont les cartes qu'on compare en cas d'égalité*)
-	   |CarteHaute of valeur*valeur*valeur*valeur*valeur(*Compare la meilleur valeur et en cas d'égalité on regarde la prochaine plus forte et ainci de suite*);;
+type comb = QuinteFlush of rang
+	   |Carre of rang*rang(*suite de 4*)
+	   |Full of rang*rang(*suite de 3*)
+	   |Couleur of rang*rang*rang*rang*rang (*en cas d'égalité faut verifier la carte suivante*)
+	   |Suite of rang
+	   |Brelan of rang*rang*rang
+	   |DoublePair of rang*rang*rang (*1er rang -> 1er paire, 2éme rang -> 2éme paire, 3éme -> derniére carte*)
+	   |Pair of rang*rang*rang*rang(*1er rang -> 1er paire, le reste des rangs sont les cartes qu'on compare en cas d'égalité*)
+	   |CarteHaute of rang*rang*rang*rang*rang(*Compare la meilleur rang et en cas d'égalité on regarde la prochaine plus forte et ainci de suite*);;
