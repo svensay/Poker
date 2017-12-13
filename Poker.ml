ab6f07bb070c604d39ca2ffd8b87d85e616828b4
Sven <svensay@gmail.com>
2017-12-13 12:22:18 +0100

    compute_comb debut

--- Poker.ml
+++ Poker.ml
@@ -27,10 +27,32 @@ let compare_comb c1 c2 = match c1 with
   | CarteHaute (r1,r2,r3,r4,r5) -> print_endline("CarteHaute")
 ;;
 
-let compute_comb d t = ;;
+(*Pas fini*)
+let compute_comb d t =
+  let l = Array.make 13 0
+  and coeur = Array.make 13 false
+  and pique = Array.make 13 false
+  and trefle = Array.make 13 false
+  and carreau = Array.make 13 false
+  and card = list_card d t 
+  and let rec aux c =
+  match c with
+  |[] -> ()
+  |h::tl -> match h with
+            |
+;;
+
+let list_card d t =
+  let l = [] in
+  match d with
+  | Main (c1,c2) -> match t with
+             | Flop (c3,c4,c5) -> (c1::(c2::(c3::(c4::(c5::l)))))
+             | Turn (c3,c4,c5,c6) -> (c1::(c2::(c3::(c4::(c5::(c6::l))))))
+             | River (c3,c4,c5,c6,c7) -> (c1::(c2::(c3::(c4::(c5::(c6::(c7::l)))))))
+;;
   
 let test1 = QuinteFlush(Valeur(10));;
-let test2 =  QuinteFlush(Valeur(9));;
+let test2 = QuinteFlush(Valeur(9));;
 
 compare_comb test1 test2;;  
              
